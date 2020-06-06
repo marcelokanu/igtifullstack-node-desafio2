c55c9e6f4ea45d17498033d01ffb6f72ff0487e9
marcelokanu <marcelokanu@gmail.com>
2020-06-06 10:23:26 -0400

    default

--- src/app.js
+++ src/app.js
@@ -7,21 +7,59 @@ const app = express();
 app.use(express.json());
 
 const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const appendFile = util.promisify(fs.appendFile);
 
-let grades = [];
+const gradesPathJson = path.resolve(__dirname, "database", "grades.json");
+
+let gradesJson = [];
 
 async function start() {
-  grades = JSON.parse(await loadJsonGrade());
+  gradesJson = JSON.parse(await loadJsonGrade());
+  gradesNextId = gradesJson.nextId;
 }
 
-function loadJsonGrade () {
-  return readFile(path.resolve(__dirname, "database", "grades.json"));
-};
+function loadJsonGrade() {
+  return readFile(gradesPathJson);
+}
+
+async function createGrade(data) {
+  const { student, subject, type, value } = data;
+
+  if (!student || !subject || !type || !value ) {
+    return {error: "student, subject, type, value são campos obrigatórios"};
+  }
+  const nextJsonId = gradesJson.nextId;
+  try {
+    const item = {
+      id: nextJsonId,
+      student,
+      subject,
+      type,
+      value,
+      timestamp: new Date(),
+    };
+
+    const gradesUpdate = [...gradesJson.grades, item];
+    const nextId = nextJsonId+1;
+
+    writeFile(gradesPathJson, JSON.stringify({nextId, gradesUpdate}), err => console.log(err));
+    
+    return item;
+  } catch (err) {
+    return err;
+  }
+}
+
 start();
 
 app.get("/", async (_, res) => {
-  
-  return res.json(grades);
+  return res.json(gradesJson);
+});
+
+app.post("/create", async (req, res) => {
+  const data = await createGrade(req.body);
+  return res.json(data);
 });
 
 module.exports = app;
